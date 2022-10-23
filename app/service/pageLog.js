b05447ed059f7245e86b3d5509e81ae57faa721d
Liu Ji <mynameisliuji@foxmail.com>
2022-10-24 02:09:54 +0800

    pageLog.js

--- app/service/pageLog.js
+++ app/service/pageLog.js
@@ -12,22 +12,36 @@ const actionDataScheme = Object.freeze({
   selectItemListFromLogFile: {
     type: 'object',
     additionalProperties: true,
-    required: [],
+    required: ['logFile'],
     properties: {
-      // table: { type: 'string' },
+      // ${appId}.page.json.log 校验
+      logFile: { type: "string", pattern: "(.)page(.)json(.)log"},
     },
   },
 });
 
 class PageLogService extends Service {
 
+  async selectLogFileList() {
+    const { config } = this.ctx.app;
+    const { baseDir, appId } = config;
+    const targetLogDirPath = path.join(baseDir, '/logs');
+    const fileList = fs.readdirSync(targetLogDirPath);
+    const pageLogFileList = fileList.filter(filename => filename.includes(`${appId}.page.json.log`));
+    const rows = pageLogFileList.map(filename => {
+      return { filename }
+    })
+    return { rows };
+  }
+
+
   async selectItemListFromLogFile() {
     const actionData = this.ctx.request.body.appData.actionData;
     validateUtil.validate(actionDataScheme.selectItemListFromLogFile, actionData);
     const { config } = this.ctx.app;
     const { baseDir, appId } = config;
-    // const { xxx } = actionData;
-    const targetLogFilePath = path.join(baseDir, `/logs/${appId}.page.json.log`);
+    const { logFile } = actionData;
+    const targetLogFilePath = path.join(baseDir, `/logs/${logFile}`);
     let strData = fs.readFileSync(targetLogFilePath, 'UTF-8').toString();
     strData = strData.replace(/"}\n/g, '"},');
     const rows = eval("[" + strData + "]")
