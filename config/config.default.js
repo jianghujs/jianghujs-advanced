01bfc5d3c187c52a8a2712bd0cb02d8b464688df
colin <colin>
2024-04-16 13:19:16 +0800

    appTitle

--- config/config.default.js
+++ config/config.default.js
@@ -13,7 +13,7 @@ module.exports = appInfo => {
   return {
     appId,
     debug: false,
-    appTitle: '江湖进阶',
+    appTitle: '江湖演示-进阶',
     appLogo: `${appId}/public/img/logo.png`,
     appType: 'single', // single: 单应用; multiApp: 多应用
     appDirectoryLink: '/',
